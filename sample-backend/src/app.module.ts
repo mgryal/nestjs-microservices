09d74441715de9eb1f2170894e10b3b3d7d590f1
mgryal <mgonzalez@outlier.cl>
2024-01-18 01:04:44 -0300

    feat: add analytics microservice

--- sample-backend/src/app.module.ts
+++ sample-backend/src/app.module.ts
@@ -11,6 +11,11 @@ import { ClientsModule, Transport } from '@nestjs/microservices';
         transport: Transport.TCP,
         options: { port: 3001 },
       },
+      {
+        name: 'ANALYTICS',
+        transport: Transport.TCP,
+        options: { port: 3003 },
+      },
     ]),
   ],
   controllers: [AppController],
